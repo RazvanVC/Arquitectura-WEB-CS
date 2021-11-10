543ad99f0c7a907c2d92e495d337cb04d4230175
RazvanVC <razvanvc@yahoo.com>
2021-11-10 16:04:55 +0100

    Listas

--- Practica4/RestauranteV1/main2.js
+++ Practica4/RestauranteV1/main2.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     //Add button click handler
     $('#btn-add').click(function(){
+        //Get the value from the table
+        $('#listaPlatos').append('<li>' + $('#plato').val() + '</li>');
         $('#listaPlatos').append('<li>Hola</li>');
         
     });
