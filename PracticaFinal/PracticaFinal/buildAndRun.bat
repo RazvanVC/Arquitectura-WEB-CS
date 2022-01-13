97d266ddf8a21d37cd467509c04a890bbcd83ca1
RazvanVC <razvanvc@yahoo.com>
2022-01-13 17:08:37 +0100

    Subida Inicial

--- PracticaFinal/PracticaFinal/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.uah/PracticaFinal .
-call docker rm -f PracticaFinal
-call docker run -d -p 9080:9080 -p 9443:9443 --name PracticaFinal com.uah/PracticaFinal
