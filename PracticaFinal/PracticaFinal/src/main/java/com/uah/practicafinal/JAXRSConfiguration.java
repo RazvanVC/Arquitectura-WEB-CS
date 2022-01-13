02289ad19b8784dad0d5fdc7dad9dda24211d17e
RazvanVC <razvanvc@yahoo.com>
2022-01-13 11:23:45 +0100

    Initial Commit

---
+++ PracticaFinal/PracticaFinal/src/main/java/com/uah/practicafinal/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package com.uah.practicafinal;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
